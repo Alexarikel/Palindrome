1c51cf12f31fec5f934e8beb1a3a0dc02c008f38
Aleksandra <alexarikel@gmail.com>
2022-03-24 17:48:56 +0200

    Palindrome version 1

---
+++ palindrome.py
@@ -0,0 +1,17 @@
+what = input ("Enter a three-digit number: ")
+
+a = int(what)%10
+
+
+b = int(what)/10
+
+c = int(b)%10
+d = int(b/10)
+
+if a == d:
+    print("The number you entered is a palindrome")
+
+else:
+    print("The number you entered is not a palindrome")
+
+
