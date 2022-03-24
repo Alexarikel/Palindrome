6f91ff31936e6221822c1ddc2c6f36b48b92dc56
Aleksandra <alexarikel@gmail.com>
2022-03-24 19:51:01 +0200

    added functionality for four-digits

--- palindrome.py
+++ palindrome.py
@@ -1,18 +1,50 @@
 # This only works with three-digits numbers
-what = input ("Enter a three-digit number: ")
+what = input ("Enter a three- or four-digit number: ")
 
-a = int(what)%10
+if int(what) > 99 and int(what) < 1000:
+
+    print("starting check for three digits")
+    a = int(what)%10
 
 
-b = int(what)/10
+    b = int(what)/10
 
-c = int(b)%10
-d = int(b/10)
+    c = int(b)%10
+    d = int(b)/10
 
-if a == d:
-    print("The number you entered is a palindrome")
+   
+
+    if a == int(d):
+        print("The number you entered is a palindrome")
+
+    else:
+        print("The number you entered is not a palindrome")
+
+
+if int(what) >= 1000 and int(what) <10000:
+
+    
+    a = int(what)%10
+
+
+    b = int(what)/10
+
+    c = int(b)%10
+    d = int(b)/10
+    e = int(d)%10
+    f = int(d)/10
+
+
+    if a == int(f) and int(c) == e:
+        print("The number you entered is a palindrome")
+
+    else:
+        print("The number you entered is not a palindrome")
 
 else:
-    print("The number you entered is not a palindrome")
+    print("Incorrect number")
+        
+
+
 
 
