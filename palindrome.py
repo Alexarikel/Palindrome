6ada4fd7c5a43d6823d86fb0953dcda19bc1ebc4
Aleksandra <alexarikel@gmail.com>
2022-03-29 00:05:58 +0300

    final version with str slices

--- palindrome.py
+++ palindrome.py
@@ -1,37 +1,12 @@
 
-
 while True:
 
-
-    what = input ("Enter a three- or four-digit number: ")
-    what = int(what)
-
-    if what > 99 and what < 1000: 
-
-        if what%10 == int(what/100):
-
-            print("The number you entered is a palindrome")
-
-        else:
-            print("The number you entered is not a palindrome")
-
-    elif what >= 1000 and what <10000:
-        a = what%10
-        b = what/10
-        c = int(b)%10
-        d = int(b)/10
-        e = int(d)%10
-        f = int(d)/10
-
-        if a == int(f) and int(c) == e:
-            print("The number you entered is a palindrome")
-
-        else:
-            print("The number you entered is not a palindrome")
-
+    what = input("Enter a number or a word: ")
+    
+    if what[::1] == what[::-1]:
+        print("The input you entered is a palindrome")
     else:
-        print("Incorrect number")
-            
+        print("The input you entered is not a palindrome")
 
 
 
