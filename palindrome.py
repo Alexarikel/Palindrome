d780286568cabb7cef1a36caf4d5d5ef054980f9
Aleksandra <alexarikel@gmail.com>
2022-03-31 16:18:56 +0300

    really final version

--- palindrome.py
+++ palindrome.py
@@ -1,12 +1,6 @@
-
 while True:
-
     what = input("Enter a number or a word: ")
-    
-    if what[::1] == what[::-1]:
-        print("The input you entered is a palindrome")
-    else:
-        print("The input you entered is not a palindrome")
+    print("The input you entered is a palindrome" if what[::1] == what[::-1] else "The input you entered is not a palindrome")
 
 
 
